33ff1d17dc51edb0902b4338675bee9a3cb798df
Lutfullo08 <lutfullorizayev222@gmail.com>
2026-02-15 18:28:18 +0500

    65645

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import get_info, create_product, list_product, detail_product, update_product, putch_product, delete_product
+# from .views import get_info, create_product, list_product, detail_product, update_product, putch_product, delete_product
+from .views import ProductListView, ProductCreateView, ProductDetailView, ProductDelateView, ProductUpdateView
 
 urlpatterns = [
-    path('get-info/', get_info),
-    path('create/', create_product),
-    path('list/', list_product),
-    path('detail/<int:pk>', detail_product),
-    path('update/<int:pk>', update_product),
-    path('detail/<int:pk>', putch_product),
-    path('delete/<int:pk>', delete_product)
+    path('list/', ProductListView.as_view()),
+    path('create/', ProductCreateView.as_view()),
+    path('detail/<int:pk>', ProductDetailView.as_view()),
+    path('delete/<int:pk>', ProductDelateView.as_view()),
+    path('update/<int:pk>', ProductUpdateView.as_view()),
 ]
+
+# from rest_framework import routers
+# from .views import ProductViewSet
+#
+# router = routers.SimpleRouter()
+# router.register(r'product-w', ProductViewSet)
+# urlpatterns = router.urls
--- products/views.py
+++ products/views.py
@@ -1,115 +1,240 @@
 from django.core.serializers import serialize
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import Product, ProductSerializer
 from rest_framework.exceptions import ValidationError
 from .models import Product
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, RetrieveAPIView, \
+    UpdateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
 
 
 
-@api_view(['GET'])
-def get_info(request):
-    data = {
-        "success": True,
-        "message": "Hammasi joyida"
-    }
-    return Response(data)
 
-@api_view(['POST'])
-def create_product(request):
-    serializer = ProductSerializer(data = request.data)
-    if serializer.is_valid():
+
+
+class ProductListView(APIView):
+    def get(self, request):
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many=True)
+        data = {
+            'status': status.HTTP_200_OK,
+            'message': 'products',
+            'data': serializer.data
+        }
+        return Response(data)
+
+class ProductCreateView(APIView):
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                'status': status.HTTP_201_CREATED,
+                'message': 'Products',
+                'data': serializer.data
+            }
+            return Response(data)
+        data = {
+            'status': status.HTTP_400_BAD_REQUEST,
+            'message': 'error',
+            'desc': serializer.errors
+        }
+        return Response(data)
+
+class ProductUpdateView(APIView):
+    def put(self, request, pk):
+        product = Product.objects.filter(pk=pk).first()
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
         data = {
-            "success": True,
-            "message": "product yaratildi",
-            "data": serializer.data
+            'status': status.HTTP_200_OK,
+            'message': 'product update',
+            'data':serializer.data
         }
-
         return Response(data)
-    raise ValidationError(serializer.errors)
 
-
-@api_view(['get'])
-def list_product(request):
-    products = Product.objects.all()[::-1]
-    if len(products) == 0:
-        raise ValidationError("Malumot yo'q")
-
-    serializer = ProductSerializer(products, many=True)
-
-    data = {
-        "success": True,
-        "message": "products",
-        "data": serializer.data
-    }
-    return Response(data)
-
-
-@api_view(['GET'])
-def detail_product(request,pk):
-    products = Product.objects.filter(pk=pk).first()
-    if products is None:
-        raise ValidationError("Malumot Topilmadi")
-    serializer = ProductSerializer(products)
-
-    data = {
-        "success": True,
-        "message": "product",
-        "data": serializer.data
-    }
-    return Response(data)
-
-
-@api_view(['POST'])
-def update_product(request, pk):
-    products = Product.objects.filter(pk=pk).first()
-    if products is None:
-        raise ValidationError("Malumot Topilmadi")
-
-    serializer = ProductSerializer(data = request.data)
-    if serializer.is_valid():
+    def patch(self, request, pk):
+        product = Product.objects.filter(pk=pk).first()
+        serializer = ProductSerializer(product, request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
-
         data = {
-            "success": True,
-            "message": "product yaratildi",
-            "data": serializer.data
+            'status': status.HTTP_200_OK,
+            'message': 'product update',
+            'data':serializer.data
         }
-
         return Response(data)
-    raise ValidationError(serializer.errors)
 
 
-
-@api_view(['PUTCH'])
-def putch_product(request, pk):
-    products = Product.objects.filter(pk=pk).first()
-    if products is None:
-        raise ValidationError("Malumot Topilmadi")
-
-    serializer = ProductSerializer(data = request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-
+class ProductDetailView(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
         data = {
-            "success": True,
-            "message": "product yaratildi",
-            "data": serializer.data
+            'status': status.HTTP_200_OK,
+            'data': serializer.data
         }
-
         return Response(data)
-    raise ValidationError(serializer.errors)
 
-@api_view(['DELETE',])
-def delete_product(request,pk):
-    product = Product.objects.filter(pk=pk).first()
-    if product is None:
-        raise ValidationError('Malumot topilmadi')
-    product.delete()
-    data = {
-            'success': True,
-            'massege': 'produc ochirildi',
+
+
+class ProductDelateView(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        data = {
+            'status': status.HTTP_204_NO_CONTENT,
+            'message': 'Product delete'
         }
-    return Response(data)
+        return Response(data)
+
+
+
+# class ProductViewSet(viewsets.ModelViewSet):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+
+
+# class ProductListView(ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#
+#
+# class ProductCreateView(CreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# class ProductDetailView(RetrieveUpdateDestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# class ProductDelateView(DestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+
+# class ProductUpdateView(UpdateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+
+
+
+
+
+
+
+
+#
+# @api_view(['GET'])
+# def get_info(request):
+#     data = {
+#         "success": True,
+#         "message": "Hammasi joyida"
+#     }
+#     return Response(data)
+#
+# @api_view(['POST'])
+# def create_product(request):
+#     serializer = ProductSerializer(data = request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         data = {
+#             "success": True,
+#             "message": "product yaratildi",
+#             "data": serializer.data
+#         }
+#
+#         return Response(data)
+#     raise ValidationError(serializer.errors)
+#
+#
+# @api_view(['get'])
+# def list_product(request):
+#     products = Product.objects.all()[::-1]
+#     if len(products) == 0:
+#         raise ValidationError("Malumot yo'q")
+#
+#     serializer = ProductSerializer(products, many=True)
+#
+#     data = {
+#         "success": True,
+#         "message": "products",
+#         "data": serializer.data
+#     }
+#     return Response(data)
+#
+#
+# @api_view(['GET'])
+# def detail_product(request,pk):
+#     products = Product.objects.filter(pk=pk).first()
+#     if products is None:
+#         raise ValidationError("Malumot Topilmadi")
+#     serializer = ProductSerializer(products)
+#
+#     data = {
+#         "success": True,
+#         "message": "product",
+#         "data": serializer.data
+#     }
+#     return Response(data)
+#
+#
+# @api_view(['POST'])
+# def update_product(request, pk):
+#     products = Product.objects.filter(pk=pk).first()
+#     if products is None:
+#         raise ValidationError("Malumot Topilmadi")
+#
+#     serializer = ProductSerializer(data = request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#
+#         data = {
+#             "success": True,
+#             "message": "product yaratildi",
+#             "data": serializer.data
+#         }
+#
+#         return Response(data)
+#     raise ValidationError(serializer.errors)
+#
+#
+#
+# @api_view(['PUTCH'])
+# def putch_product(request, pk):
+#     products = Product.objects.filter(pk=pk).first()
+#     if products is None:
+#         raise ValidationError("Malumot Topilmadi")
+#
+#     serializer = ProductSerializer(data = request.data, partial=True)
+#     if serializer.is_valid():
+#         serializer.save()
+#
+#         data = {
+#             "success": True,
+#             "message": "product yaratildi",
+#             "data": serializer.data
+#         }
+#
+#         return Response(data)
+#     raise ValidationError(serializer.errors)
+#
+# @api_view(['DELETE',])
+# def delete_product(request,pk):
+#     product = Product.objects.filter(pk=pk).first()
+#     if product is None:
+#         raise ValidationError('Malumot topilmadi')
+#     product.delete()
+#     data = {
+#             'success': True,
+#             'massege': 'produc ochirildi',
+#         }
+#     return Response(data)
